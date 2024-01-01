cba36d517fa30dd73dc21a3ce7cb23a827e8472b
ilkiinz <aqayev2205@gmail.com>
2024-01-01 15:28:47 +0400

    hello world

--- main.js
+++ main.js
@@ -9,27 +9,26 @@ let toShowSecond = document.querySelector('.second-currency');
 let toShowResult = document.querySelector('.final-result');
 
 function convertCurrency(event) {
-  event.preventDefault();
-  let amount = amountInput.value;
-  let from = baseCurrencyInput.value;
-  let to = secondCurrencyInput.value;
-  let result = 0;
+    event.preventDefault();
+    let amount = parseInt(amountInput.value);
+    let from = baseCurrencyInput.value;
+    let to = secondCurrencyInput.value;
+    let result = 0;
   
-  try{
-    if (from == to){
-      result = amount;
-    } else {
-     result = amount * crrncy[from][to];
-  }
-  }
-  catch(err) {
-    result = amount * (1 / crrncy[to][from]);
-  }
+    try {
+        if (from == to) {
+            result = amount;
+        } else {
+            result = amount * crrncy[from][to];
+        }
+    } catch(err) {
+        result = amount * (1 / crrncy[to][from]);
+    }
   
-  toShowAmount.innerHTML = amount;
-  toShowBase.textContent = from + ' = ';
-  toShowSecond.textContent = to;
-  toShowResult.textContent = result; 
+    toShowAmount.innerHTML = amount;
+    toShowBase.textContent = from + ' = ';
+    toShowSecond.textContent = to;
+    toShowResult.textContent = parseInt(result);
 }
 
 btn.addEventListener('click', convertCurrency);
